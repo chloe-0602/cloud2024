8a664fb06ec920dfa8115425a14ea904a5e9530b
HerryWEu <yihong.wu@mhp.com>
2024-11-08 23:25:06 +0800

    feat: 微服务调用者订单模块,增加调用其他的Pay模块接口

--- cloud-consumer-order80-stu/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80-stu/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.atguigu.cloud.controller;
 import com.atguigu.cloud.entities.PayDTO;
 import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -19,13 +16,45 @@ import org.springframework.web.client.RestTemplate;
  * @Version 1.0
  */
 @RestController
+@RequestMapping("/consumer")
 public class OrderController {
     @Resource
     private RestTemplate restTemplate;
-    public static final String PAY_URL="http://localhost:8001";
+    public static final String PAY_URL = "http://localhost:8001";
 
-    @GetMapping("/consumer/pay/add")
-    public ResultData addPayApi(PayDTO payDTO){
+    /**
+     * 只能是Get请求
+     *
+     * @param payDTO
+     * @return
+     */
+    @GetMapping("/pay/add")
+    public ResultData addOrder(PayDTO payDTO) {
         return restTemplate.postForObject(PAY_URL + "/pay/add", payDTO, ResultData.class);
     }
+
+    @GetMapping("/pay/get/{id}")
+    public ResultData getPayInfo(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(PAY_URL + "/pay/get/" + id, ResultData.class, id);
+    }
+
+    /**
+     * 看官网， put方法没有返回值
+     *
+     * @param payDTO
+     * @return
+     */
+    @GetMapping("/pay/update")
+    public ResultData getPayInfo(PayDTO payDTO) {
+        restTemplate.put(PAY_URL + "/pay/update", payDTO);
+        return ResultData.success(null);
+    }
+
+
+    @GetMapping("/pay/del/{id}")
+    public ResultData delPayById(@PathVariable("id") Integer id) {
+        restTemplate.delete(PAY_URL + "/pay/del/" + id, ResultData.class, id);
+        return ResultData.success(null);
+    }
+
 }
