b9e07b275b17a9a0e83db442689d6f1d1945c44b
HerryWEu <yihong.wu@mhp.com>
2024-11-09 12:08:50 +0800

    feat: 负载均衡Loadbalancer,默认负载均衡算法是轮询 此时切换为 随机；其实企业里面一般轮询也就够了

--- cloud-consumer-order80-stu/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80-stu/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
@@ -1,8 +1,15 @@
 package com.atguigu.cloud.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -15,10 +22,20 @@ import org.springframework.web.client.RestTemplate;
  * @Version 1.0
  */
 @Configuration
+@LoadBalancerClient(
+        //下面的value值大小写一定要和consul里面的名字一样，必须一样
+        value = "cloud-payment-service",configuration = RestTemplateConfig.class)
 public class RestTemplateConfig {
     @Bean
     @LoadBalanced //Consul默认支持负载均衡，不加就会报错java.net.UnkownHostException
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
     }
 }
