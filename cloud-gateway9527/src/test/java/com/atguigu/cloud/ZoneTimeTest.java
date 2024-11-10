be86959576d73efbc1f5cd418901a1fc2c1d2ddf
HerryWEu <yihong.wu@mhp.com>
2024-11-10 20:39:04 +0800

    feat: Gateway： 常用断言1 After Route Predicate

---
+++ cloud-gateway9527/src/test/java/com/atguigu/cloud/ZoneTimeTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * ClassName: ZoneTimeTest
+ * Package: com.atguigu.cloud
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/11/10 19:13
+ * @Version 1.0
+ */
+public class ZoneTimeTest {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
