c4bd9ab5c7fa0a9c3adb9093cce695cfd20e18f4
HerryWEu <yihong.wu@mhp.com>
2024-11-10 08:52:04 +0800

    feat: Openfeign, 高级特性-重试机制，默认是不重试，此时换成重试3次

---
+++ cloud-consumer-feign-order80-stu/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.cloud.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: FeignConfig
+ * Package: com.atguigu.cloud.config
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/11/10 8:48
+ * @Version 1.0
+ */
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Retryer retryer(){
+//        return Retryer.NEVER_RETRY;
+        return new Retryer.Default(100, 1,3);
+    }
+}
